b533412ef8b169052276d09c82349ca039d846d9
sugandhsec <sugandhsec112@gmail.com>
2022-09-20 11:33:36 +0530

    soemchanges in amazon header

--- Amazon/app_buyer/models.py
+++ Amazon/app_buyer/models.py
@@ -4,20 +4,23 @@ from django.db import models
 from app_seller.models import Product
 
 # Create your models here.
+
+
 class User(models.Model):
-    fullname=models.CharField(max_length=50)
-    email=models.EmailField(unique=True)
-    mobile=models.CharField(max_length=50)
-    password=models.CharField(max_length=50)
-    pic=models.FileField(upload_to='buyer_images',default="anonymous.png")
+    fullname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    pic = models.FileField(upload_to='buyer_images', default="anonymous.png")
 
     def __str__(self):
         return self.email
 
+
 class Cart(models.Model):
-    userid=models.ForeignKey(User,on_delete=models.CASCADE)
-    prodcutid=models.ForeignKey(Product,on_delete=models.CASCADE)
-    orderid=models.IntegerField()
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    prodcutid = models.ForeignKey(Product, on_delete=models.CASCADE)
+    orderid = models.IntegerField()
 
     def __str__(self):
         return str(self.orderid)
